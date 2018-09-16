212c95f54c19364afbbfbaf4a281c359d94389d9
oyvindsabo <oyvind.sabo@gmail.com>
2018-09-16 20:38:49 +0200

    Initial commit
    Game kind of works, but the sorting sorts different direction every time lol

---
+++ sortGame.js
@@ -0,0 +1,84 @@
+class SortGame {
+  
+  constructor(width, height) {
+    this.width = width;
+    this.height = height || width;
+    this.board = [];
+    for (let y = 0; y < height; y++) {
+      this.board.push([]);
+      for (let x = 1; x <= width; x++) {
+        this.board[y].push(x+y*width)
+      }
+    }
+  }
+
+  /*****************************************************************************
+   * UTILITY METHODS
+   ****************************************************************************/
+
+  toListOfColumns (listOfRows) {
+    let listOfColumns = [];
+    for (let x = 0; x < listOfRows[0].length; x++) {
+      listOfColumns.push([]);
+      for (let y = 0; y < listOfRows.length; y++) {
+        listOfColumns[x].push(listOfRows[y][x]);
+      }
+    }
+    return listOfColumns;
+  }
+
+  toListOfRows (listOfColumns) {
+    let listOfRows = [];
+    for (let y = 0; y < listOfColumns[0].length; y++) {
+      listOfRows.push([]);
+      for (let x = 0; x < listOfColumns.length; x++) {
+        listOfRows[y].push(listOfColumns[x][y]);
+      }
+    }
+    return listOfRows;
+  }
+
+  equalArrays (array1, array2) {
+    return array1.map((e, i) => e === array2[i] ? 0 : 1)
+                 .reduce((a, b) => a + b) === 0;
+  }
+
+  /*****************************************************************************
+   * PUBLIC API
+   ****************************************************************************/
+
+  getBoard () {
+    return this.board;
+  }
+
+  getHeight () {
+    return this.height;
+  }
+
+  getWidth () {
+    return this.width;
+  }
+
+  sort (rowCol, index) {
+    if (rowCol === 'col') {
+      this.board.sort(col => col[index]);
+    } else if (rowCol === 'row') {
+      this.board = this.toListOfRows(
+                   this.toListOfColumns(
+                   this.board).sort(row => row[index]));
+    }
+  }
+
+  hasWon () {
+    const flatBoard = [].concat.apply([], this.board);
+    return this.equalArrays([...flatBoard].sort((a, b) => a - b), flatBoard);
+  }
+
+}
+/*
+let game = new SortGame(4,5);
+game.sort('col', 0);
+game.sort('row', 2);
+console.log(game.getBoard());
+console.log(game.hasWon());
+*/
